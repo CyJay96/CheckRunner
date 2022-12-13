bd947c5a7229830696cc638f6e9047ec851b883d
CyJay96 <konstantin201586@gmail.com>
2022-12-13 15:10:54 +0300

    Initial Commit

---
+++ src/main/java/com/clevertec/checkrunner/CheckRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.clevertec.checkrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckRunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clevertec/checkrunner/CheckRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clevertec.checkrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
