d1e60400bd6a70566f3259f308b6ea530199139f
CyJay96 <konstantin201586@gmail.com>
2022-12-14 23:42:23 +0300

    Added base exceptions

---
+++ src/main/java/com/clevertec/checkrunner/exception/CheckNotFoundException.java
@@ -0,0 +1,9 @@
+package com.clevertec.checkrunner.exception;
+
+public class CheckNotFoundException extends RuntimeException {
+
+    public CheckNotFoundException(String message) {
+        super(message);
+    }
+
+}
