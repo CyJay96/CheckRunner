a66e9b545c68f3cfebe1c34d4f53fbfc02e2e100
CyJay96 <konstantin201586@gmail.com>
2022-12-18 01:43:17 +0300

    Refactored code; fixed duplication

--- src/main/java/com/clevertec/checkrunner/exception/CheckNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.clevertec.checkrunner.exception;
-
-public class CheckNotFoundException extends RuntimeException {
-
-    public CheckNotFoundException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/clevertec/checkrunner/exception/CheckProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.clevertec.checkrunner.exception;
-
-public class CheckProductNotFoundException extends RuntimeException {
-
-    public CheckProductNotFoundException(String message) {
-        super(message);
-    }
-
-}
