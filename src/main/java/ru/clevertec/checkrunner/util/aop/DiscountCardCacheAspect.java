5ba1ac0d75d954c5ea78aba3932b2c036c5125f4
CyJay96 <konstantin201586@gmail.com>
2023-03-15 19:54:03 +0300

    feat: add postman collection

--- src/main/java/ru/clevertec/checkrunner/util/aop/DiscountCardCacheAspect.java
+++ src/main/java/ru/clevertec/checkrunner/util/aop/DiscountCardCacheAspect.java
@@ -27,65 +27,65 @@ public class DiscountCardCacheAspect {
         cache = factory.getCache(cacheType, cacheCapacity);
     }
 
-//    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.save(..))")
-//    public DiscountCard aroundCreateMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-//        DiscountCard discountCard = (DiscountCard) joinPoint.proceed();
-//        cache.put(discountCard.getId(), discountCard);
-//        return discountCard;
-//    }
-//
-//    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.findById(..))")
-//    public Optional<DiscountCard> aroundFindByIdMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-//        Long id = (Long) joinPoint.getArgs()[0];
-//        if (cache.containsKey(id)) {
-//            return Optional.of(cache.get(id));
-//        }
-//        Optional<DiscountCard> discountCardOptional = (Optional<DiscountCard>) joinPoint.proceed();
-//        discountCardOptional.ifPresent(discountCard -> cache.put(discountCard.getId(), discountCard));
-//        return discountCardOptional;
-//    }
-//
-//    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.deleteById(..))")
-//    public Object aroundDeleteByIdMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-//        Long id = (Long) joinPoint.getArgs()[0];
-//        if (cache.containsKey(id)) {
-//            cache.remove(id);
-//        }
-//        return joinPoint.proceed();
-//    }
-//
-//    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.delete(..))")
-//    public Object aroundDeleteMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-//        DiscountCard discountCard = (DiscountCard) joinPoint.getArgs()[0];
-//        if (cache.containsKey(discountCard.getId())) {
-//            cache.remove(discountCard.getId());
-//        }
-//        return joinPoint.proceed();
-//    }
-//
-//    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.existsByNumber(..))))")
-//    public boolean aroundExistsByNumberMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-//        Long number = (Long) joinPoint.getArgs()[0];
-//        List<DiscountCard> cacheDiscountCardList = cache.values().stream()
-//                .filter(discountCard -> number.equals(discountCard.getNumber()))
-//                .toList();
-//        if (!cacheDiscountCardList.isEmpty()) {
-//            return true;
-//        }
-//        return (boolean) joinPoint.proceed();
-//    }
-//
-//    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.findAllByNumber(..))))")
-//    public List<DiscountCard> aroundFindByNumberMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-//        Long number = (Long) joinPoint.getArgs()[0];
-//        List<DiscountCard> cacheDiscountCardList = cache.values().stream()
-//                .filter(discountCard -> number.equals(discountCard.getNumber()))
-//                .toList();
-//        if (!cacheDiscountCardList.isEmpty()) {
-//            return cacheDiscountCardList;
-//        }
-//        List<DiscountCard> discountCardList = (List<DiscountCard>) joinPoint.proceed();
-//        discountCardList.forEach(discountCard -> cache.put(discountCard.getId(), discountCard));
-//        return discountCardList;
-//    }
+    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.save(..))")
+    public DiscountCard aroundCreateMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        DiscountCard discountCard = (DiscountCard) joinPoint.proceed();
+        cache.put(discountCard.getId(), discountCard);
+        return discountCard;
+    }
+
+    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.findById(..))")
+    public Optional<DiscountCard> aroundFindByIdMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        Long id = (Long) joinPoint.getArgs()[0];
+        if (cache.containsKey(id)) {
+            return Optional.of(cache.get(id));
+        }
+        Optional<DiscountCard> discountCardOptional = (Optional<DiscountCard>) joinPoint.proceed();
+        discountCardOptional.ifPresent(discountCard -> cache.put(discountCard.getId(), discountCard));
+        return discountCardOptional;
+    }
+
+    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.deleteById(..))")
+    public Object aroundDeleteByIdMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        Long id = (Long) joinPoint.getArgs()[0];
+        if (cache.containsKey(id)) {
+            cache.remove(id);
+        }
+        return joinPoint.proceed();
+    }
+
+    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.delete(..))")
+    public Object aroundDeleteMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        DiscountCard discountCard = (DiscountCard) joinPoint.getArgs()[0];
+        if (cache.containsKey(discountCard.getId())) {
+            cache.remove(discountCard.getId());
+        }
+        return joinPoint.proceed();
+    }
+
+    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.existsByNumber(..))))")
+    public boolean aroundExistsByNumberMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        Long number = (Long) joinPoint.getArgs()[0];
+        List<DiscountCard> cacheDiscountCardList = cache.values().stream()
+                .filter(discountCard -> number.equals(discountCard.getNumber()))
+                .toList();
+        if (!cacheDiscountCardList.isEmpty()) {
+            return true;
+        }
+        return (boolean) joinPoint.proceed();
+    }
+
+    @Around("execution(* ru.clevertec.checkrunner.repository.DiscountCardRepository.findAllByNumber(..))))")
+    public List<DiscountCard> aroundFindByNumberMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        Long number = (Long) joinPoint.getArgs()[0];
+        List<DiscountCard> cacheDiscountCardList = cache.values().stream()
+                .filter(discountCard -> number.equals(discountCard.getNumber()))
+                .toList();
+        if (!cacheDiscountCardList.isEmpty()) {
+            return cacheDiscountCardList;
+        }
+        List<DiscountCard> discountCardList = (List<DiscountCard>) joinPoint.proceed();
+        discountCardList.forEach(discountCard -> cache.put(discountCard.getId(), discountCard));
+        return discountCardList;
+    }
 }
